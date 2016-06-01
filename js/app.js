1294ea6fd21de115abcf1adcd95699c962f59417
Leticia Lourenco <leticiagabriellourenco@gmail.com>
2016-05-31 18:35:15 -0700

    Add map and hardcode sample markers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-var ViewModel = function() {
+function initMap() {
+	var map_center = {lat: 34.147843, lng: -118.400035};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: map_center
+	});
 
+	var myLatLng = {lat: 34.147843, lng: -118.400035};
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: ''
+	});
+
+	var myLatLng = {lat: 34.147843, lng:  -118.391550};
+	marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: ''
+	});
 }
+
+
