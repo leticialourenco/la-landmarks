78cb903ae7f1f7a88aab3e3eead842ef8236a27e
Leticia Lourenco <leticiagabriellourenco@gmail.com>
2016-06-01 23:02:41 -0700

    Open infoWindow when location is selected from the list view

--- js/app.js
+++ js/app.js
@@ -4,75 +4,51 @@ var initialLocations = [
 	{
 		title: 'Capella Salon',
 		lat: 34.144981,
-		lng: -118.414361
+		lng: -118.414361,
+		index: 0
 	},
 	{
 		title: 'Lemonade Studio City',
 		lat: 34.143106,
-		lng: -118.403175
+		lng: -118.403175,
+		index: 1
 	},
 	{
 		title: 'Universal CityWalk',
 		lat: 34.136491,
-		lng: -118.353186
+		lng: -118.353186,
+		index: 2
 	},
 	{
 		title: 'Crossfit Studio City',
 		lat: 34.141580,
-		lng: -118.387551
+		lng: -118.387551,
+		index: 3
 	},
 	{
 		title: 'Harvard-WestLake Film Festival',
 		lat: 34.139548,
-		lng: -118.412841
+		lng: -118.412841,
+		index: 4
 	},
 	{
 		title: 'SC Tattoo and Body Piercing',
 		lat: 34.140534,
-		lng: -118.371331
+		lng: -118.371331,
+		index: 5
 	},
 	{
 		title: 'Cactus Taqueria',
 		lat: 34.150541,
-		lng: -118.379208
+		lng: -118.379208,
+		index: 6
 	}
 ];
 
-/* Sets up data model for locations
+/* Helper array to store markers objects
+ * to be used combined with the index
  */
-var Location = function(data) {
-	this.title = ko.observable(data.title);
-	this.lat = ko.observable(data.lat);
-	this.lng = ko.observable(data.lng);
-}
-
-/* Sets up viewModel
- */
-var ViewModel = function() {
-	var self = this;
-	/* Creates an empty observable array
-	 */
-	this.locationList = ko.observableArray([]);
-	/* Populates the empty observable array with Location objects
-	 */
-	initialLocations.forEach(function(locationItem) {
-		self.locationList.push( new Location(locationItem) );
-	});
-	/* Declares a variable (no parameter needed)
- 	 */
-	this.currentLocation = ko.observable();
-	/* Called when some location on the view list is clicked
-	 * sets the currentLocation to the clicked object
-	 */
-	this.openInfo = function(clickedLocation) {
-		self.currentLocation(clickedLocation);
-		console.log('called open info');
-	};
-}
-/* Links view associations with ViewModel
- */
-ko.applyBindings( new ViewModel() );
-
+var markerArray = [];
 /* Google Maps API callback
  */
 function initMap() {
@@ -120,5 +96,44 @@ function initMap() {
 			infowindow.setContent(this.title);
 			infowindow.open(map, this);
 		});
+		/* Add marker to markerArray helper
+		 */
+		markerArray.push(marker);
 	}
 }
+
+/* Sets up data model for locations
+ */
+var Location = function(data) {
+	this.title = ko.observable(data.title);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+	this.index = ko.observable(data.index);
+}
+
+/* Sets up viewModel
+ */
+var ViewModel = function() {
+	var self = this;
+	/* Creates an empty observable array
+	 */
+	this.locationList = ko.observableArray([]);
+	/* Populates the empty observable array with Location objects
+	 */
+	initialLocations.forEach(function(locationItem) {
+		self.locationList.push( new Location(locationItem) );
+	});
+	/* Declares a variable (no parameter needed)
+ 	 */
+	this.currentLocation = ko.observable();
+	/* Called when some location on the view list is clicked
+	 * triggers a click event on the clickedLocation marker
+	 */
+	this.openInfo = function(clickedLocation) {
+		google.maps.event.trigger(markerArray[clickedLocation.index()], 'click');
+	};
+}
+/* Links view associations with ViewModel
+ */
+ko.applyBindings( new ViewModel() );
+
